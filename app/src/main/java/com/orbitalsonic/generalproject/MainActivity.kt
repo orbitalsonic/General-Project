be6538b477a64425cb891f1e6a1a97c7fa6294e3
Yaqoob <social.yaqoob@gmail.com>
2022-09-18 12:05:23 +0500

    General Project

--- app/src/main/java/com/orbitalsonic/generalproject/MainActivity.kt
+++ app/src/main/java/com/orbitalsonic/generalproject/MainActivity.kt
@@ -2,10 +2,14 @@ package com.orbitalsonic.generalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
     }
 }
