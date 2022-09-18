0082e5ad2e171d80cd87578aaaee24dffc89e8c4
Yaqoob <social.yaqoob@gmail.com>
2022-09-18 12:10:48 +0500

    General Project

--- app/src/main/java/com/orbitalsonic/generalproject/MainActivity.kt
+++ app/src/main/java/com/orbitalsonic/generalproject/MainActivity.kt
@@ -9,7 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
     }
 }
