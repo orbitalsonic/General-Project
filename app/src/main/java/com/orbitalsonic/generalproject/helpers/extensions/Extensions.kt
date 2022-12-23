4fb6fbd50957b30a78cba68e32bc3c2ed999e972
Yaqoob <yaqoob@gmail.com>
2022-12-23 10:54:24 +0500

    General Project

--- app/src/main/java/com/orbitalsonic/generalproject/helpers/extensions/Extensions.kt
+++ app/src/main/java/com/orbitalsonic/generalproject/helpers/extensions/Extensions.kt
@@ -1,8 +1,17 @@
 package com.orbitalsonic.generalproject.helpers.extensions
 
+import android.app.Activity
+import android.os.Build
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentContainerView
 
 object Extensions {
 
@@ -20,5 +29,25 @@ object Extensions {
                 callback()
             }
         })
+    }
+
+    fun Activity.hideSystemUI(fcvContainerMain: FragmentContainerView) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, fcvContainerMain).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    fun Activity.showSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowCompat.setDecorFitsSystemWindows(window, true)
+            val controller = window.insetsController
+            controller?.show(WindowInsets.Type.systemBars())
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_VISIBLE)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
     }
 }
