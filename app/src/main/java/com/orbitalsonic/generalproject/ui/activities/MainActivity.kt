79672ef0ba06b33827e4e7a4b3495c575359444b
Hypersoft Inc <social.yaqoob@gmail.com>
2024-10-04 16:33:14 +0500

    General Project

--- app/src/main/java/com/orbitalsonic/generalproject/ui/activities/MainActivity.kt
+++ app/src/main/java/com/orbitalsonic/generalproject/ui/activities/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.orbitalsonic.generalproject.ui.activities
 
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.orbitalsonic.generalproject.R
-import com.orbitalsonic.generalproject.databinding.ActivityMainBinding
-import com.orbitalsonic.generalproject.ui.activities.base.BaseActivity
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
     }
 }
