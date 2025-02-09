164659452432133b98a776d797cd8a0bee63000b
Yaqoob <social.yaqoob@gmail.com>
2025-02-09 22:57:45 +0500

    General Project

--- app/src/main/java/com/orbitalsonic/generalproject/MainApplication.kt
+++ app/src/main/java/com/orbitalsonic/generalproject/MainApplication.kt
@@ -1,9 +1,11 @@
 package com.orbitalsonic.generalproject
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
