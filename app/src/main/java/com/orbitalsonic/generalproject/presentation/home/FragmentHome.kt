9d18276c5223f18561f91f88b5e95e7fd6c0b6bb
Hypersoft Inc <social.yaqoob@gmail.com>
2025-02-10 17:54:09 +0500

    General Project

--- app/src/main/java/com/orbitalsonic/generalproject/presentation/home/FragmentHome.kt
+++ app/src/main/java/com/orbitalsonic/generalproject/presentation/home/FragmentHome.kt
@@ -5,8 +5,23 @@ import com.orbitalsonic.generalproject.presentation.base.fragments.BaseFragment
 
 class FragmentHome : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
-    override fun onViewCreated() {}
+    override fun onViewCreated() {
+        setupClicks()
+    }
 
-    private fun setupClicks(){}
+    private fun setupClicks(){
+        binding.apply {
+            btnBottomNav.setOnClickListener {  }
+            btnTabLayout.setOnClickListener {  }
+            btnLanguage.setOnClickListener {  }
+            btnLocation.setOnClickListener {  }
+            btnPermission.setOnClickListener {  }
+            btnResult.setOnClickListener {  }
+            btnSettings.setOnClickListener {  }
+            btnDialogs.setOnClickListener {  }
+            btnDoubleClicks.setOnClickListener {  }
+
+        }
+    }
 
 }
